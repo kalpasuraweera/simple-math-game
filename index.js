b779bc4a13b1e776cbdb8e082588452a390b403c
unknown <kalpamadhushan18@gmail.com>
2022-05-11 16:26:56 +0530

    game genarating and answer checking

--- index.js
+++ index.js
@@ -2,3 +2,66 @@
 // index eka invivible karala game eka visible karanna
 // input anuwa game eka setup karnna
 
+
+$('#start').click( ()=>{
+    const numDigits=Number($('#digits').val());
+    const operator= $('#operator').val();
+    if(numDigits>0){
+        console.log('started');
+        $('#index').addClass('notvisible');
+        $('#game').removeClass('notvisible');
+        $('#setop').text(operator);
+        const value1=newNum(numDigits);
+        const value2=newNum(numDigits);
+        $('#value1').text(value1);
+        $('#value2').text(value2);
+        const finalAnswer= answerCal(operator,value1,value2);
+        console.log(finalAnswer); 
+        $('#check').click(()=>{
+            const userAnswer= Number($('#useranswer').val());
+            if(userAnswer==finalAnswer){
+                alert("you won");
+            }
+        });
+        
+        
+    }
+    
+});
+
+
+function newNum(numDigits){
+    let theValue='';
+    for(i=0;i<numDigits;i++){
+        theValue+=Math.floor(Math.random()*10);
+    }
+    return Number(theValue)
+
+}
+
+function answerCal(operator,value1,value2){
+    let answer=0;
+    switch (operator) {
+        case '+':
+            answer=value1+value2;
+        break;
+        case '-':
+            answer=value1-value2;
+        break;
+        case '*':
+            answer=value1*value2;
+        break;
+        case '/':
+            answer=value1/value2;
+        break;
+            
+    
+        default:
+            console.log('please select a operator');
+        break;
+    }
+
+    return answer
+
+
+}
